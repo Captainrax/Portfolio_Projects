20d683cdad531f7266e98e835956bc8efafc0a8a
Andreas østgård <43293574+Captainrax@users.noreply.github.com>
2020-03-20 08:27:39 +0100

    GuessingGame 1.0.0 Stable
    working "prototype"

---
+++ Project_Ideer/Console_GuessingGame/Console_GuessingGame/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Console_GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int num = random.Next(1, 101);
+
+            // Debug
+            Console.WriteLine(num);
+            Console.WriteLine(" ");
+
+            int count = 0;
+
+            while (true)
+            {
+                Console.Write("Guess Number: ");
+                int guess = 0;
+
+                try
+                {
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("thats not a number!");
+                }
+
+                count++;
+
+                if (guess > num)
+                {
+                    Console.WriteLine("smaller.");
+                }
+                else if (guess < num)
+                {
+                    Console.WriteLine("bigger.");
+                }
+                else
+                {
+                    Console.WriteLine("you win!");
+                    break;
+                }
+
+            }
+            Console.WriteLine("you guessed " + count + " times!");
+            Console.ReadKey();
+        }
+    }
+}
