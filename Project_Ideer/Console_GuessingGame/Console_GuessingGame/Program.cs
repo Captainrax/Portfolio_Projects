17ff6fd6571370a32c2cdab3d628f6fb86f6520f
Andreas østgård <43293574+Captainrax@users.noreply.github.com>
2020-03-20 12:33:14 +0100

    GuessingGame 2.0.0 Stable
    Added basic menu navigation with Arrow keys and Enter.
    game still needs text fixed.

--- Project_Ideer/Console_GuessingGame/Console_GuessingGame/Program.cs
+++ Project_Ideer/Console_GuessingGame/Console_GuessingGame/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Console_GuessingGame
 {
@@ -6,56 +8,125 @@ namespace Console_GuessingGame
     {
         static void Main(string[] args)
         {
-            GuessGame();
+            MainMenu();
         }
 
+        public static int WinStreak;
+
+        public static void MainMenu()
+        {
+            // variable to keep track of the current Item, and a simple counter.
+            short curItem = 0, c;
+
+            ConsoleKeyInfo key;
+
+            string[] menuItems = { "New Game", "Two", "Three", "Exit" };
+
+            while(true)
+            {
+                Console.Clear();
+
+                for (c = 0; c < menuItems.Length; c++)
+                {
+                    // if the current item number is variable c, tab out this option.
+                    if (curItem == c)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(menuItems[c]);
+                        Console.ResetColor();
+                    } 
+                    else // paints the other options
+                    {
+                        Console.WriteLine(menuItems[c]);
+                    }
+                }
+
+                // Waits until the user presses a key, and puts it into an object key.
+                key = Console.ReadKey(true);
+
+                if (key.Key.ToString() == "DownArrow")
+                {
+                    curItem++;
+                    if (curItem > menuItems.Length - 1) curItem = 0;
+                }
+                else if (key.Key.ToString() == "UpArrow")
+                {
+                    curItem--;
+                    if (curItem < 0) curItem = Convert.ToInt16(menuItems.Length - 1);
+                } else if (key.Key.ToString() == "Enter")
+                {
+                    switch (curItem.ToString())
+                    {
+                        case "0":
+                            GuessGame();
+                            break;
+                        case "1":
+
+                            break;
+                        case "2":
+
+                            break;
+                        case "3":
+                            Environment.Exit(0);
+                            break;
+                    }
+                }
+            }
+
+        }
         private static void GuessGame()
         {
             Random random = new Random();
 
             int num = random.Next(1, 101);
 
-            int count = 3;
+            int GuessCount = 3;
 
-            while (count > 0)
+            while (GuessCount > 0)
             {
                 Console.Clear();
 
                 // Debug
                 //Console.WriteLine(num);
 
-                Console.WriteLine("Guesses Left: " + count);
+                Console.WriteLine("Guesses Left: " + GuessCount);
                 Console.Write("Guess Number: ");
+
                 int guess = 0;
 
                 try
                 {
                     guess = Convert.ToInt32(Console.ReadLine());
+                    GuessCount--;
                 }
                 catch
                 {
                     Console.WriteLine("thats not a number!");
                 }
 
-                count--;
+                if (GuessCount < 3)
+                {
+                    if (guess > num)
+                    {
+                        Console.WriteLine("smaller.");
+                    }
+                    else if (guess < num)
+                    {
+                        Console.WriteLine("bigger.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("you win!");
+                        WinStreak++;
+                        break;
+                    }
 
-                if (guess > num)
-                {
-                    Console.WriteLine("smaller.");
-                }
-                else if (guess < num)
-                {
-                    Console.WriteLine("bigger.");
-                }
-                else
-                {
-                    Console.WriteLine("you win!");
-                    break;
                 }
 
-                if (count < 1)
+                if (GuessCount < 1)
                 {
                     Console.WriteLine("you lost!");
+                    break;
                 }
             }
             Console.ReadKey();
